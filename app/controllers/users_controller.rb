c7ab0d0c4141a67f28970c40c2f67c1817fe5221
Adam Phillipps <adam.phillipps@wgu.edu>
2017-01-20 12:34:29 -0700

    add default role to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
 
   # GET /users/new
   def new
-    @user = User.new
+    @role ||= 'Student'
+    @user = User.new role: @role
   end
 
   # GET /users/1/edit

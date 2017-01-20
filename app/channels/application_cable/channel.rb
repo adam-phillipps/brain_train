2be9e1a3bc9685b81050dd22db5c62164c950f69
Adam Phillipps <adam.phillipps@gmail.com>
2017-01-20 13:42:00 -0700

    Make pro-comedian-level-up-style words to comment

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,5 +1,10 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
-  	# huzzah, channels
+  	# huzzah, channels and also, just who do
+  	# you think you iz?  the comment is fine
+  	# but here's a change for you:
+  	# knock knock
+  	# <your reply>
+  	# _git push ..._
   end
 end
